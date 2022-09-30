ee4947fdef1c371140dedebee2ced74c51516b99
Sinchan <sinchan.tex@gmail.com>
2022-09-30 23:15:46 +0530

    first submission

---
+++ jobathon-sep/submission1.py
@@ -0,0 +1,33 @@
+# Import libraries
+from pyspark.sql.functions import *
+
+
+spark = SparkSession.builder.appName('DF').getOrCreate()
+s3_clickstream_path = 'jobathon_sample_data\jobathin_click_data.json'
+s3_login_path = 'jobathon_sample_data\jobathon_login_data.csv'
+
+def sample_function(spark, s3_clickstream_path, s3_login_path):
+
+	# Your code goes below
+	df_clickstream =  spark.read.format("json").load(s3_clickstream_path)
+	user_mapping =  spark.read.format("csv").option("header",True).load(s3_login_path)
+
+	
+
+
+
+	df_clickstream = df_clickstream.withColumn("current_date",substring(col("event_date_time"),1,10))
+	df_clickstream = df_clickstream.withColumn("number_of_clicks",lit(1)) \
+	                        .withColumn("number_of_pageloads",lit(0)) \
+	                        .withColumn("user_id",lit(1)) \
+	                        .withColumn("logged_in",lit(0)) \
+	                        .withColumn("first_url",lit("abc")) \
+	                        .withColumn("browser_id",lit("abc"))
+	df_union = df_clickstream.select("current_date","browser_id","user_id","logged_in","first_url","number_of_clicks","number_of_pageloads")
+	
+	df_union.createOrReplaceTempView("df_union_tbl")
+	
+	df_result = spark.sql("select * from df_union_tbl limit 10")
+	
+	# Return your final spark df
+	return df_result
